e6a5180bc4357cbcdfca934e42ea354f96087225
GOMEZz157 <nicksss.gomes@gmail.com>
2025-02-26 19:10:41 -0300

    inicio da responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import logo from "../images/logo.svg";
 import Button from "./Button";
-
+import Menu from "../images/icon-hamburger.svg"
 export default function Header() {
   return (
     <header className="text-sm flex justify-around items-center w-screen h-20">
       <img src={logo} alt="logo do easybank" className="w-35 h-5"/>
       <div className="w-84 h-6 flex justify-between font-light text-grayish-blue">
+        <a href=""> <img src={Menu} alt="" /></a>
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
